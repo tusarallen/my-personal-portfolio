c43b1aad99bb97811a8ec1b402d11b924a5ffafa
email <allentusar909@gmail.com>
2023-06-27 01:12:13 +0600

    implement services on my portfolio

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,5 +1,5 @@
 import { FaCheckCircle } from "react-icons/fa";
-import './Experience.css';
+import "./Experience.css";
 
 const Experience = () => {
   return (
@@ -7,35 +7,51 @@ const Experience = () => {
       <section id="experience">
         <h5>What Skills I Have</h5>
         <h2>My Experience</h2>
-
         <div className="container experience__container">
           <div className="experience__fronted">
             <h3>Frontend Development</h3>
             <div className="experience__content">
               <article className="experience__details">
-                <FaCheckCircle />
-                <h4>HTML</h4>
-                <small className="text-light">Experienced</small>
+                <FaCheckCircle className="experience__details-icon" />
+                <div>
+                  <h4>HTML</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
               </article>
               <article className="experience__details">
-                <FaCheckCircle />
-                <h4>CSS</h4>
-                <small className="text-light">Experienced</small>
+                <FaCheckCircle className="experience__details-icon" />
+                <div>
+                  <h4>CSS</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
               </article>
               <article className="experience__details">
-                <FaCheckCircle />
-                <h4>Tailwind</h4>
-                <small className="text-light">Experienced</small>
+                <FaCheckCircle className="experience__details-icon" />
+                <div>
+                  <h4>Bootstrap</h4>
+                  <small className="text-light">Intermediate</small>
+                </div>
               </article>
               <article className="experience__details">
-                <FaCheckCircle />
-                <h4>JavaScript</h4>
-                <small className="text-light">Intermediate</small>
+                <FaCheckCircle className="experience__details-icon" />
+                <div>
+                  <h4>Tailwind</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
               </article>
               <article className="experience__details">
-                <FaCheckCircle />
-                <h4>React</h4>
-                <small className="text-light">Experienced</small>
+                <FaCheckCircle className="experience__details-icon" />
+                <div>
+                  <h4>JavaScript</h4>
+                  <small className="text-light">Intermediate</small>
+                </div>
+              </article>
+              <article className="experience__details">
+                <FaCheckCircle className="experience__details-icon" />
+                <div>
+                  <h4>React</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
               </article>
             </div>
           </div>
@@ -44,24 +60,32 @@ const Experience = () => {
             <h3>Backend Development</h3>
             <div className="experience__content">
               <article className="experience__details">
-                <FaCheckCircle />
-                <h4>Node JS</h4>
-                <small className="text-light">Intermediate</small>
+                <FaCheckCircle className="experience__details-icon" />
+                <div>
+                  <h4>Node JS</h4>
+                  <small className="text-light">Intermediate</small>
+                </div>
               </article>
               <article className="experience__details">
-                <FaCheckCircle />
-                <h4>Express JS</h4>
-                <small className="text-light">Intermediate</small>
+                <FaCheckCircle className="experience__details-icon" />
+                <div>
+                  <h4>Express JS</h4>
+                  <small className="text-light">Intermediate</small>
+                </div>
               </article>
               <article className="experience__details">
-                <FaCheckCircle />
-                <h4>MongoDB</h4>
-                <small className="text-light">Experienced</small>
+                <FaCheckCircle className="experience__details-icon" />
+                <div>
+                  <h4>MongoDB</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
               </article>
               <article className="experience__details">
-                <FaCheckCircle />
-                <h4>Mongoose</h4>
-                <small className="text-light">Experienced</small>
+                <FaCheckCircle className="experience__details-icon" />
+                <div>
+                  <h4>Mongoose</h4>
+                  <small className="text-light">Intermediate</small>
+                </div>
               </article>
             </div>
           </div>
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,7 +1,112 @@
+import { FaCheck } from "react-icons/fa";
+import "./Services.css";
+
 const Services = () => {
   return (
     <div>
-      <section id="services">This is Services</section>
+      <section id="services">
+        <h5>What I Offer</h5>
+        <h2>Services</h2>
+
+        <div className="container services__container">
+          <article className="service">
+            <div className="service__head">
+              <h3>UI/UX Design</h3>
+            </div>
+            <ul className="service__list">
+              <li>
+                <FaCheck className="service__list-icon" />
+                <p>Intuitive designs and experiences.</p>
+              </li>
+              <li>
+                <FaCheck className="service__list-icon" />
+                <p>Engaging UI, optimized interactions.</p>
+              </li>
+              <li>
+                <FaCheck className="service__list-icon" />
+                <p>Simplified tasks, intuitive interfaces.</p>
+              </li>
+              <li>
+                <FaCheck className="service__list-icon" />
+                <p>Iterative feedback, optimized usability.</p>
+              </li>
+              <li>
+                <FaCheck className="service__list-icon" />
+                <p>Elegant UI/UX, creative blend.</p>
+              </li>
+            </ul>
+          </article>
+          {/* END OF UI/UX */}
+          <article className="service">
+            <div className="service__head">
+              <h3>web development</h3>
+            </div>
+            <ul className="service__list">
+              <li>
+                <FaCheck className="service__list-icon" />
+                <p>Responsive web design.</p>
+              </li>
+              <li>
+                <FaCheck className="service__list-icon" />
+                <p>Cross-browser compatibility ensured.</p>
+              </li>
+              <li>
+                <FaCheck className="service__list-icon" />
+                <p>Full-stack development expertise.</p>
+              </li>
+              <li>
+                <FaCheck className="service__list-icon" />
+                <p>Interactive JavaScript applications.</p>
+              </li>
+              <li>
+                <FaCheck className="service__list-icon" />
+                <p>Modern HTML5 and CSS3.</p>
+              </li>
+              <li>
+                <FaCheck className="service__list-icon" />
+                <p>Seamless API integrations.</p>
+              </li>
+              <li>
+                <FaCheck className="service__list-icon" />
+                <p>Optimized website performance.</p>
+              </li>
+            </ul>
+          </article>
+          {/* CONTENT CREATION*/}
+          <article className="service">
+            <div className="service__head">
+              <h3>Content Creation</h3>
+            </div>
+            <ul className="service__list">
+              <li>
+                <FaCheck className="service__list-icon" />
+                <p>Strategic content planning.</p>
+              </li>
+              <li>
+                <FaCheck className="service__list-icon" />
+                <p>Engaging creative narratives.</p>
+              </li>
+              <li>
+                <FaCheck className="service__list-icon" />
+                <p>Platform-specific content tailoring.</p>
+              </li>
+              <li>
+                <FaCheck className="service__list-icon" />
+                <p>Persuasive copywriting skills.</p>
+              </li>
+              <li>
+                <FaCheck className="service__list-icon" />
+                <p>Timely content calendar management.</p>
+              </li>
+              <li>
+                <FaCheck className="service__list-icon" />
+                <p>Data-driven performance analysis.</p>
+              </li>
+            </ul>
+          </article>
+          {/* END OF CONTENT CREATION*/}
+        </div>
+      </section>
     </div>
   );
 };
