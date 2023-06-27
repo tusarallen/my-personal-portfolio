97312418047ad9a13a7adc5faab60be007e70512
email <allentusar909@gmail.com>
2023-06-27 23:56:15 +0600

    implement testimoinials part without swiper

--- src/components/Testimonials/Testimonialss.jsx
+++ src/components/Testimonials/Testimonialss.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
 import "./Testimonialss.css";
-
 const Testimonialss = () => {
   return (
     <div>
@@ -9,54 +8,62 @@ const Testimonialss = () => {
         <h2>Testimonials</h2>
 
         <div className="container testimonials__container">
-          <artical className="testimonial">
-            <div className="client__avater">
-              <img src="https://i.ibb.co/v3qRKD0/avatar1.jpg" alt="Avater" />
-            </div>
-            <h5 className="client__name">Elle Fanning</h5>
-            <small className="client__review">
-              Impressed by Tushar's ability to transform my vision into reality.
-              Their exceptional communication and expertise resulted in stunning
-              outcomes. Highly recommended for exceptional creativity and
-              professionalism!
-            </small>
-          </artical>
-          <artical className="testimonial">
-            <div className="client__avater">
-              <img src="https://i.ibb.co/3vsYzTz/avatar3.jpg" alt="Avater" />
-            </div>
-            <h5 className="client__name">Hamid Yaddani</h5>
-            <small className="client__review">
-              Impressed by Tushar's ability to transform my vision into reality.
-              Their exceptional communication and expertise resulted in stunning
-              outcomes. Highly recommended for exceptional creativity and
-              professionalism!
-            </small>
-          </artical>
-          <artical className="testimonial">
-            <div className="client__avater">
-              <img src="https://i.ibb.co/VjwyzLH/avatar4.jpg" alt="Avater" />
-            </div>
-            <h5 className="client__name">Ena Maxwell</h5>
-            <small className="client__review">
-              Impressed by Tushar's ability to transform my vision into reality.
-              Their exceptional communication and expertise resulted in stunning
-              outcomes. Highly recommended for exceptional creativity and
-              professionalism!
-            </small>
-          </artical>
-          <artical className="testimonial">
-            <div className="client__avater">
-              <img src="https://i.ibb.co/XbDSdkW/avatar2.jpg" alt="Avater" />
-            </div>
-            <h5 className="client__name">John Fibonki</h5>
-            <small className="client__review">
-              Impressed by Tushar's ability to transform my vision into reality.
-              Their exceptional communication and expertise resulted in stunning
-              outcomes. Highly recommended for exceptional creativity and
-              professionalism!
-            </small>
-          </artical>
+          <div className="bejal">
+            <artical className="testimonial">
+              <div className="client__avater">
+                <img src="https://i.ibb.co/v3qRKD0/avatar1.jpg" alt="Avater" />
+              </div>
+              <h5 className="client__name">Elle Fanning</h5>
+              <small className="client__review">
+                Impressed by Tushar's ability to transform my vision into
+                reality. Their exceptional communication and expertise resulted
+                in stunning outcomes. Highly recommended for exceptional
+                creativity and professionalism!
+              </small>
+            </artical>
+          </div>
+          <div className="bejal">
+            <artical className="testimonial">
+              <div className="client__avater">
+                <img src="https://i.ibb.co/3vsYzTz/avatar3.jpg" alt="Avater" />
+              </div>
+              <h5 className="client__name">Hamid Yaddani</h5>
+              <small className="client__review">
+                Impressed by Tushar's ability to transform my vision into
+                reality. Their exceptional communication and expertise resulted
+                in stunning outcomes. Highly recommended for exceptional
+                creativity and professionalism!
+              </small>
+            </artical>
+          </div>
+          <div className="bejal">
+            <artical className="testimonial">
+              <div className="client__avater">
+                <img src="https://i.ibb.co/VjwyzLH/avatar4.jpg" alt="Avater" />
+              </div>
+              <h5 className="client__name">Ena Maxwell</h5>
+              <small className="client__review">
+                Impressed by Tushar's ability to transform my vision into
+                reality. Their exceptional communication and expertise resulted
+                in stunning outcomes. Highly recommended for exceptional
+                creativity and professionalism!
+              </small>
+            </artical>
+          </div>
+          <div className="bejal">
+            <artical className="testimonial">
+              <div className="client__avater">
+                <img src="https://i.ibb.co/XbDSdkW/avatar2.jpg" alt="Avater" />
+              </div>
+              <h5 className="client__name">John Fibonki</h5>
+              <small className="client__review">
+                Impressed by Tushar's ability to transform my vision into
+                reality. Their exceptional communication and expertise resulted
+                in stunning outcomes. Highly recommended for exceptional
+                creativity and professionalism!
+              </small>
+            </artical>
+          </div>
         </div>
       </section>
     </div>
