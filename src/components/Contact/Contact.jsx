9695f5af4a5f9b85e7409b1ccee1c1614b93b70d
email <allentusar909@gmail.com>
2023-06-28 14:11:01 +0600

    implement contact section in portfolio

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,65 @@
+/* eslint-disable react/jsx-no-target-blank */
+import "./Contact.css";
+import { IoMail } from "react-icons/io5";
+import { FaFacebookMessenger, FaWhatsapp } from "react-icons/fa";
+
 const Contact = () => {
   return (
     <div>
-      <section id="contact">This is Contact</section>
+      <section id="contact">
+        <h5>Get In Touch</h5>
+        <h2>Contact Me</h2>
+
+        <div className="container contact__container">
+          <div className="contact__options">
+            <article className="contact__option">
+              <IoMail style={{ fontSize: "25px" }} />
+              <h4>Email</h4>
+              <h5>allentusar909@gmail.com</h5>
+              <a href="mailto:allentusar909@gmail.com">Send a Message</a>
+            </article>
+            <article className="contact__option">
+              <FaFacebookMessenger style={{ fontSize: "25px" }} />
+              <h4>Messenger</h4>
+              <h5>TU SH ER</h5>
+              <a
+                href="https://web.facebook.com/messages/t/100008829903217/"
+                target="_blank"
+              >
+                Send a Message
+              </a>
+            </article>
+            <article className="contact__option">
+              <FaWhatsapp style={{ fontSize: "25px" }} />
+              <h4>WhatsApp</h4>
+              <h5>+01781607315</h5>
+              <a
+                href="https://web.whatsapp.com/send?phone=+01781607315"
+                target="_blank"
+              >
+                Send a Message
+              </a>
+            </article>
+          </div>
+          {/* END OF CONTACT SECTION */}
+          <form action="">
+            <input
+              type="text"
+              name="name"
+              placeholder="Your Full Name"
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Your Email"
+              required
+            />
+            <textarea name="message" rows="7" required></textarea>
+            <button type="submit" className="btn btn-primary">Send Message</button>
+          </form>
+        </div>
+      </section>
     </div>
   );
 };
--- src/components/Testimonials/Testimonialss.jsx
+++ src/components/Testimonials/Testimonialss.jsx
@@ -1,5 +1,37 @@
 /* eslint-disable react/no-unescaped-entities */
 import "./Testimonialss.css";
+import { Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+
+const array = [
+  {
+    avatar: "https://i.ibb.co/v3qRKD0/avatar1.jpg",
+    name: "Elle Fanning",
+    review:
+      "Impressed by Tushar's ability to transform my vision into reality. Their exceptional communication and expertise resultedin stunning outcomes. Highly recommended for exceptional creativity and professionalism!",
+  },
+  {
+    avatar: "https://i.ibb.co/3vsYzTz/avatar3.jpg",
+    name: "Hamid Yaddani",
+    review:
+      "Efficient, responsive, and talented— Tushar made working together a breeze. They brought a fresh perspective to my project, exceeding expectations. Highly recommended for outstanding results and performance!",
+  },
+  {
+    avatar: "https://i.ibb.co/VjwyzLH/avatar4.jpg",
+    name: "Ena Maxwell",
+    review:
+      "Tushar is a master in their field! Professional, reliable, and a pleasure to work with. They understood my requirements perfectly, delivering exceptional outcomes. Highly recommended for outstanding services.!",
+  },
+  {
+    avatar: "https://i.ibb.co/XbDSdkW/avatar2.jpg",
+    name: "John Fibonki",
+    review:
+      "Satisfaction guaranteed with Tushar's flawless work. They captured my brand essence perfectly, delivering outstanding creativity within the agreed timeline. Highly endorsed for exceptional results and qualityfull productvity.!",
+  },
+];
+
 const Testimonialss = () => {
   return (
     <div>
@@ -7,64 +39,27 @@ const Testimonialss = () => {
         <h5>Review From Clients</h5>
         <h2>Testimonials</h2>
 
-        <div className="container testimonials__container">
-          <div className="bejal">
-            <artical className="testimonial">
-              <div className="client__avater">
-                <img src="https://i.ibb.co/v3qRKD0/avatar1.jpg" alt="Avater" />
-              </div>
-              <h5 className="client__name">Elle Fanning</h5>
-              <small className="client__review">
-                Impressed by Tushar's ability to transform my vision into
-                reality. Their exceptional communication and expertise resulted
-                in stunning outcomes. Highly recommended for exceptional
-                creativity and professionalism!
-              </small>
-            </artical>
-          </div>
-          <div className="bejal">
-            <artical className="testimonial">
-              <div className="client__avater">
-                <img src="https://i.ibb.co/3vsYzTz/avatar3.jpg" alt="Avater" />
-              </div>
-              <h5 className="client__name">Hamid Yaddani</h5>
-              <small className="client__review">
-                Impressed by Tushar's ability to transform my vision into
-                reality. Their exceptional communication and expertise resulted
-                in stunning outcomes. Highly recommended for exceptional
-                creativity and professionalism!
-              </small>
-            </artical>
-          </div>
-          <div className="bejal">
-            <artical className="testimonial">
-              <div className="client__avater">
-                <img src="https://i.ibb.co/VjwyzLH/avatar4.jpg" alt="Avater" />
-              </div>
-              <h5 className="client__name">Ena Maxwell</h5>
-              <small className="client__review">
-                Impressed by Tushar's ability to transform my vision into
-                reality. Their exceptional communication and expertise resulted
-                in stunning outcomes. Highly recommended for exceptional
-                creativity and professionalism!
-              </small>
-            </artical>
-          </div>
-          <div className="bejal">
-            <artical className="testimonial">
-              <div className="client__avater">
-                <img src="https://i.ibb.co/XbDSdkW/avatar2.jpg" alt="Avater" />
-              </div>
-              <h5 className="client__name">John Fibonki</h5>
-              <small className="client__review">
-                Impressed by Tushar's ability to transform my vision into
-                reality. Their exceptional communication and expertise resulted
-                in stunning outcomes. Highly recommended for exceptional
-                creativity and professionalism!
-              </small>
-            </artical>
-          </div>
-        </div>
+        <Swiper
+          className="container testimonials__container"
+          modules={[Pagination]}
+          spaceBetween={40}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+        >
+          {array.map(({ avatar, name, review }, index) => {
+            return (
+              <SwiperSlide key={index} className="bejal">
+                <article className="testimonial">
+                  <div className="client__avater">
+                    <img src={avatar} alt="Avater" />
+                  </div>
+                  <h5 className="client__name">{name}</h5>
+                  <small className="client__review">{review}</small>
+                </article>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </section>
     </div>
   );
